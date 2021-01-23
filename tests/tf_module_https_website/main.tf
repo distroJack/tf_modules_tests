36b552a264e9acef6c31dfb26452405f0c8975cd
Jack Humphries <jack.humphries@distrojack.com>
2021-01-23 13:35:30 -0500

    fix https website test and add profile param

--- tests/tf_module_https_website/main.tf
+++ tests/tf_module_https_website/main.tf
@@ -4,12 +4,13 @@ provider "aws" {
   region = "us-east-1"
 }
 
-module "static-site" {
-  source                  = "../../https_website"
-  base_domain             = "d4iry.com"
-  static_path             = "./static_files"
+module "https-static-site" {
+  source      = "git@github.com:distroJack/tf_module_https_website.git"
+  base_domain = "d4iry.com"
+  static_path = "./static_files"
+  profile     = "default"
 }
 
 output "test_out" {
-  value = module.static-site.domain_bucket
+  value = module.https-static-site.domain_bucket
 }
